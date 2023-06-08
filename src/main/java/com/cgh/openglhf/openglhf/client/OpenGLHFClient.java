e23440c10fbc4e24cf7289cb2a20c6a7a793d2fa
Emanuel Mairoll <accounts@emanuelmairoll.at>
2023-06-08 22:10:40 +0200

    Create the HUD base panel

--- src/main/java/com/cgh/openglhf/openglhf/client/OpenGLHFClient.java
+++ src/main/java/com/cgh/openglhf/openglhf/client/OpenGLHFClient.java
@@ -4,17 +4,26 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.FontStorage;
+import net.minecraft.client.font.GlyphRenderer;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.OrderedText;
 import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
 
+import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class OpenGLHFClient implements ClientModInitializer {
@@ -43,6 +52,7 @@ public class OpenGLHFClient implements ClientModInitializer {
         var entities = StreamSupport.stream(
                         MinecraftClient.getInstance().world.getEntities().spliterator(), false)
                 .filter(entity -> entity instanceof LivingEntity)
+                .filter(entity -> !(entity instanceof PlayerEntity))
                 .map(entity -> (LivingEntity) entity)
                 .toList();
 
@@ -84,20 +94,32 @@ public class OpenGLHFClient implements ClientModInitializer {
         double d = dispatcher.getSquaredDistanceToCamera(entity);
         if (!(d > 4096.0)) {
             boolean bl = !entity.isSneaky();
-            float f = entity.getHeight() + 0.5F;
-            int i = "deadmau5".equals(text.getString()) ? -10 : 0;
+            float f = entity.getHeight() / 2;
+            int y = "deadmau5".equals(text.getString()) ? -10 : 0;
             matrices.push();
             matrices.translate(0.0F, f, 0.0F);
             matrices.multiply(dispatcher.getRotation());
             matrices.scale(-0.025F, -0.025F, 0.025F);
             Matrix4f matrix4f = matrices.peek().getPositionMatrix();
-            float g = MinecraftClient.getInstance().options.getTextBackgroundOpacity(0.25F);
-            int j = (int) (g * 255.0F) << 24;
-            float h = (float) (-textRenderer.getWidth((StringVisitable) text) / 2);
-            textRenderer.draw(text, h, (float) i, 553648127, false, matrix4f, vertexConsumers, bl ? TextRenderer.TextLayerType.SEE_THROUGH : TextRenderer.TextLayerType.NORMAL, j, light);
-            if (bl) {
-                textRenderer.draw((Text) text, h, (float) i, -1, false, matrix4f, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0, light);
-            }
+            float rectAlpha = MinecraftClient.getInstance().options.getTextBackgroundOpacity(0.25F);
+            float x = (float) (-textRenderer.getWidth((StringVisitable) text) / 2);
+
+            textRenderer.draw((Text) text, x, (float) y - 45, -1, false, matrix4f, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0, light);
+
+            var rectMinX = -30;
+            var rectMinY = 30;
+            var rectMaxX = 31;
+            var rectMaxY = -46;
+            var rectZ = 0.01f;
+            var rectRed = 0;
+            var rectGreen = 0;
+            var rectBlue = 0;
+
+            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getTextBackground());
+            vertexConsumer.vertex(matrix4f, rectMinX, rectMinY, rectZ).color(rectRed, rectGreen, rectBlue, rectAlpha).light(light).next();
+            vertexConsumer.vertex(matrix4f, rectMaxX, rectMinY, rectZ).color(rectRed, rectGreen, rectBlue, rectAlpha).light(light).next();
+            vertexConsumer.vertex(matrix4f, rectMaxX, rectMaxY, rectZ).color(rectRed, rectGreen, rectBlue, rectAlpha).light(light).next();
+            vertexConsumer.vertex(matrix4f, rectMinX, rectMaxY, rectZ).color(rectRed, rectGreen, rectBlue, rectAlpha).light(light).next();
 
             matrices.pop();
         }
