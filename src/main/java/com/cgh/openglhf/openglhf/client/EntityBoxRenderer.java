65f3a925fd19f7836f5a8e7f4804d1aa50d925e6
Emanuel Mairoll <accounts@emanuelmairoll.at>
2023-06-12 19:27:40 +0200

    Tweaks and fixes

--- src/main/java/com/cgh/openglhf/openglhf/client/EntityBoxRenderer.java
+++ src/main/java/com/cgh/openglhf/openglhf/client/EntityBoxRenderer.java
@@ -2,8 +2,10 @@ package com.cgh.openglhf.openglhf.client;
 
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
 import org.lwjgl.opengl.GL33;
 import org.lwjgl.system.MemoryUtil;
 
@@ -53,7 +55,7 @@ public class EntityBoxRenderer {
 
         var vertices = StreamSupport.stream(entities.spliterator(), false)
                 .filter(e -> e instanceof SheepEntity)
-                .flatMapToDouble(e -> boxMinMaxToVertices(e.getBoundingBox()))
+                .flatMapToDouble(e -> boxMinMaxToVertices(e, worldRenderContext.tickDelta()))
                 .toArray();
 
         var vertexBufferData = MemoryUtil.memAllocFloat(vertices.length);
@@ -68,7 +70,7 @@ public class EntityBoxRenderer {
 
         shaderProgram.bind();
 
-        GL33.glDrawArrays(GL33.GL_LINES, 0, vertices.length / 2);
+        GL33.glDrawArrays(GL33.GL_LINES, 0, vertices.length / 3);
 
         shaderProgram.unbind();
 
@@ -84,14 +86,6 @@ public class EntityBoxRenderer {
         GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, 0);
     }
 
-    public void cleanup() {
-        GL33.glDisableVertexAttribArray(0);
-        unbindBuffers();
-        GL33.glDeleteBuffers(vbo);
-        GL33.glDeleteVertexArrays(vao);
-        shaderProgram.cleanup();
-    }
-
     private float[] doublesToFloat(double[] array) {
         float[] inFloatForm = new float[array.length];
         for (int i = 0; i < array.length; i++)
@@ -99,86 +93,16 @@ public class EntityBoxRenderer {
         return inFloatForm;
     }
 
-    private DoubleStream boxToLineVertices(Box box) {
 
-        // TODO lerp
-        double maxX = box.maxX;
-        double maxY = box.maxY;
-        double maxZ = box.maxZ;
-
-        double minX = box.minX;
-        double minY = box.minY;
-        double minZ = box.minZ;
+    private DoubleStream boxMinMaxToVertices(Entity entity, float tickDelta) {
+        var lerpX = MathHelper.lerp(tickDelta, entity.lastRenderX, entity.getX()) - entity.getX();
+        var lerpY = MathHelper.lerp(tickDelta, entity.lastRenderY, entity.getY()) - entity.getY();
+        var lerpZ = MathHelper.lerp(tickDelta, entity.lastRenderZ, entity.getZ()) - entity.getZ();
+        var box = entity.getBoundingBox().offset(lerpX, lerpY, lerpZ);
 
         return DoubleStream.of(
-                maxX, maxY, maxZ,
-                maxX, minY, maxZ,
-
-                maxX, minY, maxZ,
-                maxX, minY, minZ,
-
-                maxX, minY, minZ,
-                maxX, maxY, minZ,
-
-                maxX, maxY, minZ,
-                maxX, maxY, maxZ,
-
-                maxX, maxY, maxZ,
-                minX, maxY, maxZ,
-
-                minX, maxY, maxZ,
-                minX, maxY, minZ,
-
-                minX, maxY, minZ,
-                maxX, maxY, minZ,
-
-                minX, maxY, maxZ,
-                minX, minY, maxZ,
-
-                minX, minY, maxZ,
-                maxX, minY, maxZ,
-
-                minX, minY, maxZ,
-                minX, minY, minZ,
-
-                minX, minY, minZ,
-                maxX, minY, minZ,
-
-                minX, minY, minZ,
-                minX, maxY, minZ
+                box.minX, box.minY, box.minZ,
+                box.maxX, box.maxY, box.maxZ
         );
-    }
-
-    private DoubleStream boxMinMaxToVertices(Box box) {
-        return DoubleStream.of(
-                box.maxX, box.maxY, box.maxZ,
-                box.minX, box.minY, box.minZ
-        );
-    }
-
-    private DoubleStream calculateVerticesFromBox(Box box) {
-
-        // for now, get vertices that make up "front" facing rectangle
-        // of the bounding box
-        //
-        //       ________
-        //      /       /|
-        //     /_______/ |  ___ upper right
-        //     |      |  |
-        //     |      |  |
-        //     |      | /
-        //     |______|/
-        //     ^   ^------ "front" facing rectangle
-        //     |
-        //      ---------- lower left
-        //
-
-        return DoubleStream.of(
-                box.minX, box.minY, box.maxZ, // lower left
-                box.minX, box.maxY, box.maxZ, // upper left
-                box.maxX, box.maxY, box.maxZ, // upper right
-                box.maxX, box.minY, box.maxZ // lower right
-        );
-
     }
 }
--- src/main/resources/assets/OpenGLHF/shaders/box.geom
+++ src/main/resources/assets/OpenGLHF/shaders/box.geom
@@ -19,49 +19,52 @@ uniform mat4 ProjMat;
 
 void main()
 {
-    vec4 maxBox = gl_in[0].gl_Position;
-    vec4 minBox = gl_in[1].gl_Position;
+    vec4 minBox = gl_in[0].gl_Position;
+    vec4 maxBox = gl_in[1].gl_Position;
 
-    vec4 upperRightBack = ProjMat * ModelViewMat * vec4(minBox.x, maxBox.y, maxBox.z, 1.0);
-    vec4 upperLeftBack = ProjMat * ModelViewMat * vec4(maxBox);
-    vec4 lowerRightBack = ProjMat * ModelViewMat * vec4(minBox.x, minBox.y, maxBox.z, 1.0);
-    vec4 lowerLeftBack = ProjMat * ModelViewMat * vec4(maxBox.x, minBox.y, maxBox.z, 1.0);
-
-    vec4 upperRightFront = ProjMat * ModelViewMat * vec4(minBox.x, maxBox.y, minBox.z, 1.0);
-    vec4 upperLeftFront = ProjMat * ModelViewMat * vec4(maxBox.x, maxBox.y, minBox.z, 1.0);
-    vec4 lowerRightFront = ProjMat * ModelViewMat * vec4(minBox);
-    vec4 lowerLeftFront = ProjMat * ModelViewMat * vec4(maxBox.x, minBox.y, minBox.z, 1.0);
-
-
+    vec4 upperRightBack = vec4(minBox.x, maxBox.y, maxBox.z, 1.0);
+    vec4 upperLeftBack = vec4(maxBox.x, maxBox.y, maxBox.z, 1.0);
+    vec4 lowerRightBack =  vec4(minBox.x, minBox.y, maxBox.z, 1.0);
+    vec4 lowerLeftBack =  vec4(maxBox.x, minBox.y, maxBox.z, 1.0);
+    vec4 upperRightFront =  vec4(minBox.x, maxBox.y, minBox.z, 1.0);
+    vec4 upperLeftFront = vec4(maxBox.x, maxBox.y, minBox.z, 1.0);
+    vec4 lowerRightFront = vec4(minBox.x, minBox.y, minBox.z, 1.0);
+    vec4 lowerLeftFront = vec4(maxBox.x, minBox.y, minBox.z, 1.0);
     vec4 v[8] = vec4[8](upperRightBack, upperLeftBack, lowerRightBack, lowerLeftBack, upperRightFront, upperLeftFront, lowerRightFront, lowerLeftFront);
 
-    float minX = 1.0, maxX = -1.0, minY = 1.0, maxY = -1.0, w = 0;
+    float inf = 1.0 / 0.0;
+    float minX = inf, maxX = -inf, minY = inf, maxY = -inf;
 
     for (int i = 0; i < 8; i++) {
-        maxX = max(maxX, v[i].x);
-        minX = min(minX, v[i].x);
-        maxY = max(maxY, v[i].y);
-        minY = min(minY, v[i].y);
-        w += v[i].w;
+        vec4 screenCoords = ProjMat * ModelViewMat * v[i];
+
+        if (screenCoords.w < 0.0) {
+            continue;
+        }
+
+        vec4 normalized = screenCoords / screenCoords.w;
+
+        minX = min(minX, normalized.x);
+        maxX = max(maxX, normalized.x);
+        minY = min(minY, normalized.y);
+        maxY = max(maxY, normalized.y);
     }
 
-    w /= 8;
-
-    gl_Position = vec4(minX, maxY, 0.0, w);
+    gl_Position = vec4(minX, maxY, 0.0, 1.0);
     EmitVertex();
-    gl_Position = vec4(maxX, maxY, 0.0, w);
+    gl_Position = vec4(maxX, maxY, 0.0, 1.0);
     EmitVertex();
-    gl_Position = vec4(maxX, minY, 0.0, w);
+    gl_Position = vec4(maxX, minY, 0.0, 1.0);
     EmitVertex();
-    gl_Position = vec4(minX, minY, 0.0, w);
+    gl_Position = vec4(minX, minY, 0.0, 1.0);
     EmitVertex();
-    gl_Position = vec4(minX, maxY, 0.0, w);
+    gl_Position = vec4(minX, maxY, 0.0, 1.0);
     EmitVertex();
     EndPrimitive();
 
-
-
-
-
-
 }
+
+
+
+
+
