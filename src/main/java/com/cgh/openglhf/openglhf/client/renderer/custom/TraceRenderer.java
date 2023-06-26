5448566def410da691b21bcc2699587f53a52a46
Emanuel Mairoll <accounts@emanuelmairoll.at>
2023-06-26 19:00:58 +0200

    Worsen tracers implementation to the one of typical hacked clients

--- src/main/java/com/cgh/openglhf/openglhf/client/renderer/custom/TraceRenderer.java
+++ src/main/java/com/cgh/openglhf/openglhf/client/renderer/custom/TraceRenderer.java
@@ -2,6 +2,7 @@ package com.cgh.openglhf.openglhf.client.renderer.custom;
 
 import com.cgh.openglhf.openglhf.client.ShaderProgram;
 import com.cgh.openglhf.openglhf.client.Utils;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.util.math.MathHelper;
@@ -15,7 +16,6 @@ public class TraceRenderer extends AbstractPosRenderer {
     protected ShaderProgram makeShader() throws Exception {
         ShaderProgram shaderProgram = new ShaderProgram();
         shaderProgram.createVertexShader(Utils.loadResource("/assets/OpenGLHF/shaders/tracers.vert"));
-        shaderProgram.createGeometryShader(Utils.loadResource("/assets/OpenGLHF/shaders/tracers.geom"));
         shaderProgram.createFragmentShader(Utils.loadResource("/assets/OpenGLHF/shaders/tracers.frag"));
         shaderProgram.link();
         return shaderProgram;
@@ -23,17 +23,32 @@ public class TraceRenderer extends AbstractPosRenderer {
 
     @Override
     protected DoubleStream toVertices(Entity entity, float tickDelta) {
-        if (entity instanceof SheepEntity) {
-            return DoubleStream.of(
-                    MathHelper.lerp(tickDelta, entity.lastRenderX, entity.getX()),
-                    MathHelper.lerp(tickDelta, entity.lastRenderY, entity.getY()),
-                    MathHelper.lerp(tickDelta, entity.lastRenderZ, entity.getZ()));
-        } else {
+        var player = MinecraftClient.getInstance().player;
+        if (player == null) {
             return DoubleStream.of();
         }
+
+        var lerpedEntityX = MathHelper.lerp(tickDelta, entity.lastRenderX, entity.getX());
+        var lerpedEntityY = MathHelper.lerp(tickDelta, entity.lastRenderY, entity.getY());
+        var lerpedEntityZ = MathHelper.lerp(tickDelta, entity.lastRenderZ, entity.getZ());
+
+
+        var lerpedPlayerX = MathHelper.lerp(tickDelta, player.lastRenderX, player.getX());
+        var lerpedPlayerY = MathHelper.lerp(tickDelta, player.lastRenderY, player.getY());
+        var lerpedPlayerZ = MathHelper.lerp(tickDelta, player.lastRenderZ, player.getZ());
+
+        var crosshairX = lerpedPlayerX + player.getRotationVector().x;
+        var crosshairY = lerpedPlayerY + player.getEyeHeight(player.getPose()) + player.getRotationVector().y;
+        var crosshairZ = lerpedPlayerZ + player.getRotationVector().z;
+
+        return DoubleStream.of(
+                lerpedEntityX, lerpedEntityY, lerpedEntityZ,
+                crosshairX, crosshairY, crosshairZ
+        );
+
     }
 
     protected void glDrawArrays(int length) {
-        GL33.glDrawArrays(GL33.GL_POINTS, 0, length);
+        GL33.glDrawArrays(GL33.GL_LINES, 0, length);
     }
 }
